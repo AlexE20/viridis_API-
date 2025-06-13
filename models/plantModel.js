c7f78acea8dbbe43c88188a0fd7d57df53a7d852
AlexE20 <00009423@uca.edu.sv>
2025-06-12 21:37:24 -0600

    :bug:Fixing bugs

--- models/plantModel.js
+++ models/plantModel.js
@@ -5,21 +5,8 @@ class Plant {
     this.scientific_name = data.scientific_name || [];
     this.care_level = data.care_level;
     this.watering = data.watering;
-    this.watering_general_benchmark = {
-      unit: data.watering_general_benchmark?.unit || null,
-      value: data.watering_general_benchmark?.value || null
-    };
-    this.default_image = {
-      license: data.default_image?.license,
-      license_name: data.default_image?.license_name,
-      license_url: data.default_image?.license_url,
-      thumbnail: data.default_image?.thumbnail,
-      small_url: data.default_image?.small_url,
-      regular_url: data.default_image?.regular_url,
-      medium_url: data.default_image?.medium_url,
-      original_url: data.default_image?.original_url
-    };
-    this.other_images = data.other_images;
+    this.default_image = data.medium_url;
+    this.recommendations = data.care_guide || []; 
   }
 }
 
--- models/userPlantModel.js
+++ models/userPlantModel.js
@@ -1,31 +1,14 @@
-const Plant = require("./plantModel");
-
 class UserPlant {
-    constructor(id, plantData) {
+    constructor(id, data) {
         this.id = id;
-        this.common_name = plantData.common_name;
-        this.scientific_name = plantData.scientific_name || [];
-        this.care_level = plantData.care_level;
-        this.watering = plantData.watering;
-
-        this.watering_general_benchmark = {
-            unit: plantData.watering_general_benchmark?.unit || null,
-            value: plantData.watering_general_benchmark?.value || null
-        };
-
-        this.default_image = {
-            license: plantData.default_image?.license,
-            license_name: plantData.default_image?.license_name,
-            license_url: plantData.default_image?.license_url,
-            thumbnail: plantData.default_image?.thumbnail,
-            small_url: plantData.default_image?.small_url,
-            regular_url: plantData.default_image?.regular_url,
-            medium_url: plantData.default_image?.medium_url,
-            original_url: plantData.default_image?.original_url
-        };
-
-        this.other_images = plantData.other_images;
+        this.plant_id=data.plant_id;
+        this.common_name = data.common_name;
+        this.scientific_name = data.scientific_name || [];
+        this.care_level = data.care_level;
+        this.watering = data.watering;
+        this.default_image = data.medium_url;
+        this.recommendations = data.care_guide || [];
     }
 }
 
-module.exports=UserPlant;
+module.exports = UserPlant;
