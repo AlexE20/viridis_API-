9e15c40826479b691604f200d291950a610fb8cd
AlexE20 <00009423@uca.edu.sv>
2025-06-26 21:02:48 -0600

    :bug: Adding shade level that for some reason was deleted PD:I´m done

--- models/userPlantModel.js
+++ models/userPlantModel.js
@@ -7,6 +7,7 @@ class UserPlant {
     this.common_name = data.common_name;
     this.scientific_name = data.scientific_name || [];
     this.care_level = data.care_level;
+    this.shade_level = data.shade_level;
     this.watering = data.watering;
     this.default_image = data.default_image?.medium_url || null;
     this.recommendations = data.care_guide || [];
