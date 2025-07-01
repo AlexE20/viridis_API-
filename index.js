c4eb3b25b0a6666e6b6a9a726e6a950646ecb49d
Pablo Vides <00080323@uca.edu.sv>
2025-07-01 15:28:50 -0600

    Changing schedule time

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 
 
 
-cron.schedule("25 21 * * *", async () => {
+cron.schedule("40 21 * * *", async () => {
   console.log("🔔 Watering reminder running at 20:21 UTC (server time):", new Date().toString());
   try {
     await checkWateringReminders();
